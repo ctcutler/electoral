1f330999712522d0b4b8c528600c164d03a48ae0
ctcutler <ctcutler@gmail.com>
2016-11-20 11:35:57 -0500

    tweak styling; prototype number crunching

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { states } from './states.js';
+import R from 'ramda';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        selected: 'California',
+        personVotes: this.calcPersonVotes(states)
+    };
+  }
+
+  calcPersonVotes = R.map(R.prop('Total of citizens 18 years and older'));
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{states.Alabama["Electoral votes"]}</p>
+        <p>People in {this.state.selected} get {this.state.personVotes[this.state.selected]} votes each.</p>
       </div>
     );
   }
