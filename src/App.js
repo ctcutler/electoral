34059686f1ac1883bb00d68f87761d0389e7c02f
Chris Cutler <ctcutler@librato.com>
2016-11-25 14:31:42 -0500

    more styling and text tweaks

--- src/App.js
+++ src/App.js
@@ -15,15 +15,14 @@ const getSuggestionValue = suggestion => suggestion.name;
 const renderSuggestion = suggestion => (
   <span> {suggestion.name} </span>
 );
-const INITIAL_STATE = 'Wyoming';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
         suggestions: [],
-        value: INITIAL_STATE,
-        chosen: matchingStates(INITIAL_STATE)[0]
+        value: "",
+        chosen: null
     };
   }
 
@@ -51,7 +50,7 @@ class App extends Component {
 
     // Autosuggest will pass through all these props to the input element.
     const inputProps = {
-      placeholder: 'State Name',
+      placeholder: 'State',
       value,
       onChange: this.onChange
     };
@@ -60,7 +59,7 @@ class App extends Component {
     let votesPerPerson = '–';
     let elVotePercent = '–';
     let stateName = '–';
-    let totalVotes = '–';
+    let totalVotes = numeral(nation.votingAgePop).format('0,0');
     let statePop = '–';
     let stateVotes = '–';
     let elVotes = '–';
@@ -70,7 +69,6 @@ class App extends Component {
       const sv = er * nation.votingAgePop;
       const vpp = sv / chosen.votingAgePop;
       stateName = chosen.name;
-      totalVotes = numeral(nation.votingAgePop).format('0,0');
       statePop = numeral(chosen.votingAgePop).format('0,0');
       stateVotes = numeral(sv).format('0,0');
       elVotePercent = numeral(chosen.elVotePercent).format('0.00');
@@ -80,8 +78,11 @@ class App extends Component {
 
     return (
       <div>
+        <div className="explanation">
+          <p>How many votes does each voter get in a presidential election?  If the election were decided by a popular vote, every voter would get exactly one vote.  Instead, the <a href="https://en.wikipedia.org/wiki/Electoral_College_(United_States)">electoral college</a> decides the election.  How does it divide up the <span className="number">{totalVotes}</span> available votes?  </p>
+        </div>
         <div className="headline">
-          Each person in<br/>
+          Each&nbsp;voter&nbsp;in
           <Autosuggest
             suggestions={suggestions}
             onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
@@ -90,21 +91,17 @@ class App extends Component {
             getSuggestionValue={getSuggestionValue}
             renderSuggestion={renderSuggestion}
             inputProps={inputProps}
-          /><br/>
-          gets {votesPerPerson} votes for president.
+          />
+          gets&nbsp;{votesPerPerson}&nbsp;votes&nbsp;for&nbsp;president.
         </div>
         <div className="explanation">
-          <p>If the presidential election were decided by a popular vote, every voter would get exactly one vote.  Instead, the <a href="https://en.wikipedia.org/wiki/Electoral_College_(United_States)">electoral college</a> decides the election.  How does it divide up the <span className="number">{totalVotes}</span> available votes?</p>
+          <p>How do we know that? {stateName} gets <span className="number">{elVotes}</span> out of <span className="number">538</span> (or about <span className="number">{elVotePercent}%</span>) of the nation's electoral votes. Its voters decide about <span className="number">{elVotePercent}%</span> of the election.  That means that {stateName} gets about <span className="number">{elVotePercent}%</span> of those <span className="number">{totalVotes}</span> available votes: <span className="number">{stateVotes}</span> votes.  And {stateName} has <span className="number">{statePop}</span> voters.</p>
 
-          <p>{stateName} gets <span className="number">{elVotes}</span> out of <span className="number">538</span> (or about <span className="number">{elVotePercent}%</span>) of the nation's electoral votes. Its voters decide about <span className="number">{elVotePercent}%</span> of the election.</p>
-
-          <p>That means that {stateName} gets about <span className="number">{elVotePercent}%</span> of the <span className="number">{totalVotes}</span> votes available: <span className="number">{stateVotes}</span> votes.</p>
-
-          <p>{stateName} has <span className="number">{statePop}</span> voters.  <span className="number">{stateVotes}</span> state votes / <span className="number">{statePop}</span> state voters = <span className="number">{votesPerPerson}</span> votes per person.</p>
+<p><span className="number">{stateVotes}</span> votes / <span className="number">{statePop}</span> voters = <span className="number">{votesPerPerson}</span> votes per person.</p>
 
         </div>
         <div className="source">
-          <p>Vote counts come from the U.S. Census's 2016 Electorate Profiles data: http://www.census.gov/data/tables/time-series/demo/voting-and-registration/electorate-profiles-2016.html, specifically the "Total of citizens 18 years and older" figures. Source code available <a href="https://github.com/ctcutler/electoral">here</a>.</p>
+          <p>Vote counts come from the U.S. Census's 2016 Electorate Profiles data: <a href="http://www.census.gov/data/tables/time-series/demo/voting-and-registration/electorate-profiles-2016.html">http://www.census.gov/data/tables/time-series/demo/voting-and-registration/electorate-profiles-2016.html</a>, specifically the "Total of citizens 18 years and older" figures. Source code available <a href="https://github.com/ctcutler/electoral">here</a>.</p>
         </div>
       </div>
     );
